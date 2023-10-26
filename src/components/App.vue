04c62c09a181edcbbc1ac1bad41f99f469a0e300
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-26 16:39:26 +0900

    run i18n

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,42 @@
 <template>
-    <div id = "description">
-        <h2>What is?</h2>
-        <p>This library enables programming beginers to make an artistic (especially game) programming easily.</p>
+    <nav>
+        <LanguageSelection />
+    </nav>
+    <div id="description">
+        <h2>{{ $t("description.what_is") }}</h2>
+        <p> {{ $t("description.what_is_description") }} </p>
     </div>
 </template>
 
 <script>
-    export default {
+    export default Vue.defineComponent({
         name: 'App',
-    }
+        components: {
+            "LanguageSelection": Vue.defineAsyncComponent(
+                () => loadModule("src/components/LanguageSelection.vue", options)
+            ),
+        },
+        setup() {
+            //set up i18n
+            const { t } = VueI18n.useI18n()
+            return { t }
+        }
+    })
 </script>
+
+<i18n>
+    {
+        "en": {
+            "description": {
+                "what_is": "What is HotSoupScript?",
+                "what_is_description": "This library enables programming beginers to make an artistic (especially game) programming easily."
+            }
+        },
+        "ja": {
+            "description": {
+                "what_is": "HotSoupScriptとは？",
+                "what_is_description": "このライブラリは、プログラミング初心者が簡単にグラフィカルな（特にゲーム）プログラミングを行えるようにするものです。"
+            }
+        }
+    }
+</i18n>
