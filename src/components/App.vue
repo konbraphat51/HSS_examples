22774ab02c8ff96361a93783cee0a52f813901c2
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-26 13:49:23 +0900

    Base Vue system

---
+++ src/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>HotSoupScript Examples</h1>
+    <div id = "description">
+        <h2>What is?</h2>
+        <p>This library enables programming beginers to make an artistic (especially game) programming easily.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    }
+</script>
