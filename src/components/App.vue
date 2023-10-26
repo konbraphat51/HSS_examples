f2fb1bda0741ad2e799a005e11531e32fdb76a2e
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-26 13:52:23 +0900

    move h1

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,4 @@
 <template>
-    <h1>HotSoupScript Examples</h1>
     <div id = "description">
         <h2>What is?</h2>
         <p>This library enables programming beginers to make an artistic (especially game) programming easily.</p>
