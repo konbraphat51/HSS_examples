848203b44f3df3897465898bb6154700bfbcb218
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-10-29 22:44:13 +0900

    add: ExampleList fetches Description Data

--- src/components/ExampleList/ExampleList.vue
+++ src/components/ExampleList/ExampleList.vue
@@ -2,7 +2,7 @@
     <div id="ExampleList">
         <CategorySelector />
         <ul class="options">
-            <ExampleOption v-for="option in options" v-bind:title="option.title" v-bind:description="option.description"/>
+            <ExampleOption v-for="description in descriptions" v-bind:title="description.title" v-bind:description="description.option_description"/>
         </ul>
     </div>
 </template>
@@ -19,37 +19,24 @@
             const { t } = VueI18n.useI18n()
             return { t }
         },
-        computed: {
-            options() {
-                return [
-                    {
-                        title: this.t("options.move_by_key.title"),
-                        description: this.t("options.move_by_key.description")
-                    }
-                ]
-            }
-        }
-    })
-</script>
-
-<i18n>
-    {
-        "en": {
-            "options": {
-                "move_by_key": {
-                    "title": "Move by Key",
-                    "description": "Let your character move by pressing keys on the keyboard.\n You can learn usage of key inputing system"
-                }
+        props: {
+            selected_example: {
+                type: String,
+                required: true
+            },
+            example_descriptions: {
+                type: Object,
+                required: true
             }
         },
-
-        "ja": {
-            "options": {
-                "move_by_key": {
-                    "title": "キー入力で文字を動かす",
-                    "description": "キーボードのキーを押すことで文字を動かせます。\nキー入力システムの使い方を学べます。"
+        computed: {
+            descriptions() {
+                let output = {}
+                for (let example in this.example_descriptions) {
+                    output[example] = this.example_descriptions[example][i18n.global.locale.value]
                 }
+                return output
             }
-        }
-    }
-</i18n>
+        },
+    })
+</script>
