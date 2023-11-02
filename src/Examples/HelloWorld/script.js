9b983fe52844524a7e1dcabd3a7360fede0d94d2
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-11-02 19:20:23 +0900

    improve helloworld

--- src/Examples/HelloWorld/script.js
+++ src/Examples/HelloWorld/script.js
@@ -6,4 +6,10 @@ async function main() {
     //ja: 最後の2つの数字は、x、yの位置です。
     //ja: ""の中は何でもOKです。
     DrawText("ageuoiehbuoefa", 100, 100)
+
+    //en: When numbers, you don't need "".
+    //en: But letters needs ""
+    //ja: 数字の時は、""は不要です。
+    //ja: しかし、文字の時は""が必要です。
+    DrawText(334, 100, 200)
 }
