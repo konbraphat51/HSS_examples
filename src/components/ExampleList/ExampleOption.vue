d5ef85433acd522d0a1011a0d5a9788f569658f0
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-11-02 10:48:51 +0900

    Improve: lighten color

--- src/components/ExampleList/ExampleOption.vue
+++ src/components/ExampleList/ExampleOption.vue
@@ -38,7 +38,7 @@
         border-radius: 10px;
         padding: 10px;
         margin: 10px;
-        background-color: #5eec60;
+        background-color: #e8ffe8;
         box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
     }
 
